ebf53edfd0bc599ae9958d7436a9e694aff80052
Andrew MacMurray <a.macmurray@icloud.com>
2017-12-12 16:34:01 +0000

    reduce repetition in controller_test #9

--- test/controller_test.exs
+++ test/controller_test.exs
@@ -4,31 +4,27 @@ defmodule ControllerTest do
   alias ModelTestHelper, as: TestHelper
 
   test "Controller.parse_guess! take a user guess and return a result" do
-    expected = 3
-    actual   = Controller.parse_guess!("3\n")
-    assert actual == expected
-
-    expected = :error
-    actual = Controller.parse_guess!("hello?")
-    assert actual == expected
+    guesses = [
+      {"3\n",    3},
+      {"hello?", :error}
+    ]
+    for {guess, expected} <- guesses do
+      assert Controller.parse_guess!(guess) == expected
+    end
   end
 
   test "Controller.init should print the initial model and return the model unmodified" do
-    model = Model.init(:human_v_human)
-    actual = Controller.init(model, FakeIO)
-    assert actual == model
-
-    model = Model.init(:computer_v_computer)
-    actual = Controller.init(model, FakeIO)
-    assert actual == model
-
-    model = Model.init({:human_v_computer, :X, :player_1})
-    actual = Controller.init(model, FakeIO)
-    assert actual == model
-
-    model = Model.init({:human_v_computer, :O, :player_2})
-    actual = Controller.init(model, FakeIO)
-    assert actual == model
+    init_options = [
+      :human_v_human,
+      :computer_v_computer,
+      {:human_v_computer, :X, :player_1},
+      {:human_v_computer, :O, :player_2}
+    ]
+    for option <- init_options do
+      initial_model = Model.init(option)
+      actual        = Controller.init(initial_model, FakeIO)
+      assert actual == initial_model
+    end
   end
 
   test "Controller.handle_guess should take a game model for human_v_human,
@@ -67,20 +63,20 @@ defmodule ControllerTest do
 
   test "Controller.handle_guess should return a new model for a :human_v_computer game
         where human guesses first" do
+    fp            = FakeProcess
     initial_model = Model.init({:human_v_computer, :X, :player_1})
-    actual = Controller.handle_guess(initial_model, {IOGuess9, FakeProcess})
-    expected = Model.update(initial_model, 9)
-    assert actual == expected
-
-    initial_model = Model.init({:human_v_computer, :X, :player_1})
-    actual = Controller.handle_guess(initial_model, {IOGuessUnrecognized, FakeProcess})
-    assert actual == initial_model
-
-    initial_model =
-      Model.init({:human_v_computer, :X, :player_1})
-      |> TestHelper.sequence([5, 1])
-    actual = Controller.handle_guess(initial_model, {IOGuess5, FakeProcess})
-    assert actual == initial_model
+    updated_model = initial_model |> TestHelper.sequence([5, 1])
+    states = [
+      {{IOGuess9, fp},            initial_model, [9]},
+      {{IOGuess5, fp},            initial_model, [5]},
+      {{IOGuessUnrecognized, fp}, initial_model, []},
+      {{IOGuess5, fp},            updated_model, [5, 1]}
+    ]
+    for {effect_modules, current_model, guesses} <- states do
+      actual   = Controller.handle_guess(current_model, effect_modules)
+      expected = current_model |> TestHelper.sequence(guesses)
+      assert actual == expected
+    end
   end
 
   test "Controller.handle_guess should return a new model for a :human_v_computer game
@@ -92,39 +88,27 @@ defmodule ControllerTest do
     assert actual == expected
   end
 
-
-  test "Controller.terminus should print results and return status for human_v_human game" do
-    model = Model.init(:human_v_human) |> TestHelper.sequence([5,1,2,4,8])
-    actual = Controller.terminus(model, FakeIO)
-    assert actual == :player_1_win
-
-    model = Model.init(:human_v_human) |> TestHelper.sequence([5,1,2,8,9,3,6,4,7])
-    actual = Controller.terminus(model, FakeIO)
-    assert actual == :draw
-  end
-
-  test "Controller.terminus should print results and return status for computer_v_computer game" do
-    model = Model.init(:computer_v_computer) |> TestHelper.sequence([5,1,2,4,8])
-    actual = Controller.terminus(model, FakeIO)
-    assert actual == :player_1_win
-
-    model = Model.init(:computer_v_computer) |> TestHelper.sequence([5,1,2,8,9,3,6,4,7])
-    actual = Controller.terminus(model, FakeIO)
-    assert actual == :draw
-  end
-
-  test "Controller.terminus should print results and return status for human_v_computer game" do
-    model = Model.init({:human_v_computer, :X, :player_1}) |> TestHelper.sequence([5,1,2,8,9,3,6,4,7])
-    actual = Controller.terminus(model, FakeIO)
-    assert actual == :draw
-
-    model = Model.init({:human_v_computer, :O, :player_2}) |> TestHelper.sequence([5,1,2,3,8])
-    actual = Controller.terminus(model, FakeIO)
-    assert actual == :player_1_win
-
-    model = Model.init({:human_v_computer, :O, :player_1}) |> TestHelper.sequence([1,5,9,2,3,8])
-    actual = Controller.terminus(model, FakeIO)
-    assert actual == :player_2_win
+  test "Controller.terminus should print results and return status
+        for each type of game" do
+    win_seq   = [5,1,2,4,8]
+    win_seq_2 = [1,5,9,2,3,8]
+    draw_seq  = [5,1,2,8,9,3,6,4,7]
+    games = [
+      {:human_v_human,                     win_seq,   :player_1_win},
+      {:human_v_human,                     win_seq_2, :player_2_win},
+      {:human_v_human,                     draw_seq,  :draw},
+      {:computer_v_computer,               win_seq,   :player_1_win},
+      {:computer_v_computer,               win_seq_2, :player_2_win},
+      {:computer_v_computer,               draw_seq,  :draw},
+      {{:human_v_computer, :O, :player_2}, win_seq,   :player_1_win},
+      {{:human_v_computer, :O, :player_1}, win_seq_2, :player_2_win},
+      {{:human_v_computer, :X, :player_1}, draw_seq,  :draw}
+    ]
+    for {game_options, sequence, expected} <- games do
+      model  = Model.init(game_options) |> TestHelper.sequence(sequence)
+      actual = Controller.terminus(model, FakeIO)
+      assert actual == expected
+    end
   end
 
   test "Controller.run_game runs a game to its terminus" do
