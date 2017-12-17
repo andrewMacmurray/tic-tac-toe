f4b93529b53a9959f2d7972902796f0907fb917f
Andrew MacMurray <a.macmurray@icloud.com>
2017-12-17 09:09:04 +0000

    slightly better ab minimax, still fails on some boards

--- lib/tic_tac_toe/minimax.ex
+++ lib/tic_tac_toe/minimax.ex
@@ -6,18 +6,28 @@ defmodule TicTacToe.Minimax do
   # assigns moves a positive or negative score depending on player perspective
   # free codecamp article: https://goo.gl/Zd8eJh
   def best_move(board, ai_player) do
+    {mv, _} =
+      board
+      |> Board.possible_moves()
+      |> Enum.map(fn mv -> get_move_score(mv, board, ai_player) end)
+      |> Enum.max_by(fn {_, {_, b}} -> b end)
+    mv
+  end
+
+  def get_move_score(move, board, ai_player) do
     alpha_beta    = {-100, 100}
     player_state  = {ai_player, false}
     initial_depth = 1
-    minimax(board, initial_depth, player_state, alpha_beta)
+    board         = Board.update(board, move, ai_player)
+    {move, minimax(board, initial_depth, player_state, alpha_beta)}
   end
 
-  def minimax(board, depth, {player, is_oponent} = ps, alpha_beta) do
+  def minimax(board, depth, {player, is_oponent} = ps, {a, b} = alpha_beta) do
     win = Board.winner?(board, player)
     cond do
-      win && !is_oponent ->  1 / depth
-      win &&  is_oponent -> -1 / depth
-      Board.full?(board) ->  0
+      win && !is_oponent -> {1 / depth, b}
+      win &&  is_oponent -> {a, -1 / depth}
+      Board.full?(board) -> {a, b}
       true               -> go_again(board, depth + 1, swap_player_state(ps), alpha_beta)
     end
   end
@@ -30,27 +40,27 @@ defmodule TicTacToe.Minimax do
 
   def ab_prune(mv, board, depth, {player, is_oponent} = ps, {a, b} = ab) do
     next_board = Board.update(board, mv, player)
-    val        = minimax(next_board, depth, ps, ab)
+    {a_, b_}   = minimax(next_board, depth, ps, ab)
     if !is_oponent do
-      Enum.min([b, val]) |> handle_halt(is_oponent, ab, mv)
+      max(a_, a) |> handle_halt(is_oponent, ab)
     else
-      Enum.max([a, val]) |> handle_halt(is_oponent, ab, mv)
+      min(b_, b) |> handle_halt(is_oponent, ab)
     end
   end
 
-  def handle_halt(new_b, false, {a, _}, mv) do
-    if a >= new_b do
-      {:halt, mv}
-    else
-      {:cont, {a, new_b}}
-    end
-  end
-
-  def handle_halt(new_a, true, {_, b}, mv) do
+  def handle_halt(new_a, false, {_, b}) do
     if new_a >= b do
-      {:halt, mv}
+      {:halt, {new_a, b}}
     else
       {:cont, {new_a, b}}
+    end
+  end
+
+  def handle_halt(new_b, true, {a, _}) do
+    if a >= new_b do
+      {:halt, {a, new_b}}
+    else
+      {:cont, {a, new_b}}
     end
   end
 
